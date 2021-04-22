a0b461d2603fd75bee4705a9f76bb823ed237f92
Yunus Emre <y.emre_sacma@hotmail.com>
2021-04-22 20:39:54 +0300

    init TinderClone project

---
+++ TindeClone/TindeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TindeClone
+//
+//  Created by YUNUS EMRE SAÇMA on 22.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
